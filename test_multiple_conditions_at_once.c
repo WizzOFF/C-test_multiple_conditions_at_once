73db36b9b10119f6156a3c3a249b1a711012b403
WizzOFF <maher_off@proton.me>
2025-03-02 22:59:59 +0100

    test_multiple_conditions_at_once.c: add an OR constraint with ||

--- test_multiple_conditions_at_once.c
+++ test_multiple_conditions_at_once.c
@@ -3,7 +3,21 @@
 
 int main(int argc, char *argv[])
 {
+    /*
     if (age > 18 && age < 25)
+    */
+
+
+    // Modify with an "OR" constraint with ||
+
+    if (age > 30 || argent > 100000)
+    {
+        printf("Bienvenue chez PicsouBanque !");
+    }
+    else
+    {
+        printf("Hors de ma vue, miserable !");
+    }
      
     return 0;
 }
