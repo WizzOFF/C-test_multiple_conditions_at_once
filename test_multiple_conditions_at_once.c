8a3da32296cff3379660089aa982a5514daec5ac
WizzOFF <maher_off@proton.me>
2025-03-02 23:28:47 +0100

    test_multiple_conditions_at_once.c: make true/false conditions with booleans 0

--- test_multiple_conditions_at_once.c
+++ test_multiple_conditions_at_once.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
 
     // Test the opposite of a condition with !
-
+    /*
     if (!(age < 18))
     {
         printf("Bienvenue chez PicsouBanque !");
@@ -30,6 +30,19 @@ int main(int argc, char *argv[])
     else
     {
         printf("Hors de ma vue, miserable gone !");
+    }
+    */
+
+
+    // Make true/false conditions with booleans
+
+    if (0)
+    {
+        printf("C'est vrai");
+    }
+    else
+    {
+        printf("C'est faux");
     }
     
     return 0;
